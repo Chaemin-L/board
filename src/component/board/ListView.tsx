404f432729f5196b2442884dd2f7478cf4841cb2
Chaemin-L <chaem12@kookmin.ac.kr>
2024-09-12 05:06:41 +0900

    feat: 게시글 수정 기능 구현

--- src/component/board/ListView.tsx
+++ src/component/board/ListView.tsx
@@ -14,16 +14,22 @@ const ListView = ({ listData }: { listData: BoardType[] }) => {
 
 export default ListView;
 
-const ListItem = memo(({ data: { id, title } }: { data: BoardType }) => {
-  console.log(`/boards/${id}`);
-  return (
-    <li
-      key={id}
-      className="border border-slate-200 rounded-md p-2 transition-colors hover:bg-gray-200 hover:cursor-pointer"
-    >
-      <Link to={`/boards/${id}`} className="w-full  block">
-        {title}
-      </Link>
-    </li>
-  );
-});
+const ListItem = memo(
+  ({ data: { id, title, content } }: { data: BoardType }) => {
+    console.log(`/boards/${id}`);
+    return (
+      <li
+        key={id}
+        className="border border-slate-200 rounded-md p-2 transition-colors hover:bg-gray-200 hover:cursor-pointer"
+      >
+        <Link
+          to={`/boards/${id}`}
+          state={{ id, title, content }}
+          className="w-full  block"
+        >
+          {title}
+        </Link>
+      </li>
+    );
+  }
+);
--- src/page/board/detail/index.tsx
+++ src/page/board/detail/index.tsx
@@ -1,5 +1,70 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { lcStorage } from "../../../lib/lcStorage";
+import Input from "../../../component/Input";
+import Textarea from "../../../component/Textarea";
+import Button from "../../../component/Button";
+import { BoardType } from "../list";
+import clsx from "clsx";
+
 const BoardDetailPage = () => {
-  return <div>this is BoardDetail</div>;
+  const { state } = useLocation();
+  const [data, setData] = useState<BoardType>({});
+  const [editMode, setEditMode] = useState<boolean>(false);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const onEdit = () => {
+    const updatedListData = lcStorage
+      .get("boards")
+      .map((board: BoardType) => (board.id === data.id ? data : board));
+    lcStorage.set("boards", updatedListData);
+    setEditMode(false);
+  };
+
+  useEffect(() => {
+    setData(state);
+  }, []);
+
+  if (!state)
+    return (
+      <div>
+        잘못된 접근입니다.<Link to="/boards">게시판 목록으로 이동</Link>
+      </div>
+    );
+
+  return (
+    <div>
+      <Link to="/boards">목록으로</Link>
+      <div>
+        this is BoardDetail
+        <Input
+          label="제목"
+          name="title"
+          disabled={!editMode}
+          value={data.title}
+          onChange={onChange}
+        />
+        <Textarea
+          label="내용"
+          name="content"
+          disabled={!editMode}
+          value={data.content}
+          onChange={onChange}
+        />
+        <div className={clsx(editMode && "hidden")}>
+          <Button onClick={() => setEditMode(!editMode)}>수정</Button>
+          <Button>삭제</Button>
+        </div>
+        <div className={clsx(!editMode && "hidden")}>
+          <Button onClick={onEdit}>완료</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BoardDetailPage;
