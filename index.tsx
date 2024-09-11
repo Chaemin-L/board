e65c5eb59ba681f739f41fbfb02c64210319d210
Chaemin-L <chaem12@kookmin.ac.kr>
2024-09-12 03:22:06 +0900

    chore: tailwind 설정 추가

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./global.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import GlobalLayout from "./src/layout/global.tsx";
 import ROUTES from "./src/router/default.tsx";
+import "./global.css";
 
 const router = createBrowserRouter([
   { path: "/", element: <GlobalLayout />, children: ROUTES },
